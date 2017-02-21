0898eec6df1905671dffe44a265d6645c114351c
Geoff Shannon <geoffpshannon@gmail.com>
2017-02-20 18:58:40 -0600

    Make static site into a metalsmith built static site

---
+++ site.js
@@ -0,0 +1,20 @@
+var Metalsmith  = require('metalsmith');
+var markdown    = require('metalsmith-markdown');
+var layouts     = require('metalsmith-layouts');
+
+Metalsmith(__dirname)
+  .metadata({
+    title: "Bodies in Balance Pilates Website",
+    generator: "Metalsmith",
+    url: "http://www.bodiesinbalancepilates.com"
+  })
+  .source('./source')
+  .destination('./build')
+  .clean(false)
+  .use(markdown())
+  .use(layouts({
+    engine: 'handlebars'
+  }))
+  .build(function(err, files) {
+    if (err) { throw err; }
+  });
