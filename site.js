086f31ff968ae351f962901eb8506c8fb9f41517
Geoff Shannon <geoffpshannon@gmail.com>
2017-03-26 16:24:34 -0500

    Use the real https url in the site sites.js file

--- site.js
+++ site.js
@@ -6,7 +6,7 @@ Metalsmith(__dirname)
   .metadata({
     title: "Bodies in Balance Pilates Website",
     generator: "Metalsmith",
-    url: "http://localhost:3000"
+    url: "https://www.bodiesinbalancepilates.com"
   })
   .source('./source')
   .destination('./build')
